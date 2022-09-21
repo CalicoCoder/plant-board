7f8cab1f2bfca2852d5969bc5b9a929d6bae66d2
Rob <rob@calicocoding.co>
2022-09-21 09:42:20 -0400

    Add basic styling and logo

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,22 +1,23 @@
 import {trpc} from "../utils/trpc";
 import {NextPage} from "next";
 import {Plant} from "@prisma/client";
+import Layout from "../../components/layout";
 
 const Main: NextPage = () => {
   // const plant = trpc.useQuery(["plant.plantByNickname", {nickname: "Big ZZ"}]);
   const plants = trpc.useQuery(["plant.getAll"]);
 
   return (
-    <>
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
-        <div className="pt-6 text-2xl text-blue-500 grid gap-4 grid-cols-6 grid-rows-3">
+    <Layout>
+      <div className="container mx-auto flex flex-col items-center min-h-screen pt-8 p-4">
+        <div className="pt-6 text-slate-700 grid gap-4 grid-cols-6 grid-rows-3">
           {
             plants.data ? plants.data.plants.map(
               (plant: Plant) => (
-                <div className="bg-slate-300 rounded-lg flex flex-col justify-center items-center" key={plant.id}>
-                  <div>{plant.nickName}</div>
-                  <div>{plant.commonName}</div>
-                  <div>
+                <div className="bg-green-300 rounded-lg flex flex-col justify-center items-center shadow-lg divide-y divide-dashed divide-medium-brown" key={plant.id}>
+                  <div className="flex justify-center w-full text-center text-xl p-2">{plant.nickName ? plant.nickName : "NO NICKNAME"}</div>
+                  <div className="flex justify-center w-full text-center p-2">{plant.commonName}</div>
+                  <div className="flex justify-center w-full flex-col justify-center items-center text-sm p-2">
                     <div>{plant.waterDates.length}</div>
                     <div>{plant.waterInstructions}</div>
                   </div>
@@ -24,8 +25,8 @@ const Main: NextPage = () => {
               )) : <p>No plants stored</p>
           }
         </div>
-      </main>
-    </>
+      </div>
+    </Layout>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-brown': '#DBC8A3',
+        'medium-brown': '#C7945A',
+      },
+    },
   },
   plugins: [],
+  purge: {
+    safelist: [
+    ]
+  },
 };

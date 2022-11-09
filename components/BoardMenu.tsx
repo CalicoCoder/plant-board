ab6c0119b4e0edc1a4c012b0867d7c1ce3a61a04
Rob <rob@calicocoding.co>
2022-11-09 12:04:56 -0500

    Rollback the radix versions to resolve issue where body pointer events are ignored when dialog is inside dropdown (#20)
    Convert the dialog over to use state to open/close rather than trigger component

--- components/BoardMenu.tsx
+++ components/BoardMenu.tsx
@@ -2,7 +2,7 @@ import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import {RiMenuLine} from "react-icons/ri";
 import {IoAdd} from "react-icons/io5";
 import React from "react";
-import {DialogContent, DialogTrigger, Dialog} from "./StandardDialog";
+import {StandardDialog} from "./StandardDialog";
 import AddPlantForm from "./Forms/AddPlantForm";
 
 export default function BoardMenu(props: { refreshData: () => Promise<void> }) {
@@ -15,27 +15,27 @@ export default function BoardMenu(props: { refreshData: () => Promise<void> }) {
 
   return (
     <>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger asChild={true} className="z-10 self-baseline lg:fixed lg:left-[5%] lg:top-50 lg:drop-shadow-lg hover:lg:drop-shadow-xl">
-            <button className="bg-green-200 focus-visible:outline-none active:bg-green-300 hover:bg-green-300 rounded lg:rounded-full p-2">
-              <RiMenuLine/>
-            </button>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Portal>
-            <DropdownMenu.Content
-              className="bg-slate-100 hover:bg-slate-200 rounded-lg drop-shadow-xl animate-scaleIn origin-dropdown">
-              <DropdownMenu.Item className="menu-item">
-                <DialogTrigger asChild={true}>
-                  <button><IoAdd/><span>Add New Plant</span></button>
-                </DialogTrigger>
-              </DropdownMenu.Item>
-              <DropdownMenu.Arrow className="fill-slate-100"/>
-            </DropdownMenu.Content>
-          </DropdownMenu.Portal>
-        </DropdownMenu.Root>
-        <DialogContent><AddPlantForm onSubmitAction={handleFormSubmit}/></DialogContent>
-      </Dialog>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild={true}
+                              className="z-10 self-baseline lg:fixed lg:left-[5%] lg:top-50 lg:drop-shadow-lg hover:lg:drop-shadow-xl">
+          <button
+            className="bg-green-200 focus-visible:outline-none active:bg-green-300 hover:bg-green-300 rounded lg:rounded-full p-2">
+            <RiMenuLine/>
+          </button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            className="bg-slate-100 hover:bg-slate-200 rounded-lg drop-shadow-xl animate-scaleIn origin-dropdown">
+            <DropdownMenu.Item className="menu-item">
+              <button onClick={() => {setIsDialogOpen(true)}}><IoAdd/><span>Add New Plant</span></button>
+            </DropdownMenu.Item>
+            <DropdownMenu.Arrow className="fill-slate-100"/>
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+      <StandardDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <AddPlantForm onSubmitAction={handleFormSubmit}/>
+      </StandardDialog>
     </>
   );
 }
--- components/StandardDialog.tsx
+++ components/StandardDialog.tsx
@@ -1,19 +1,31 @@
-import React, {ReactNode} from 'react';
-import * as DialogPrimitive from '@radix-ui/react-dialog';
+import React, {Dispatch, ReactNode, SetStateAction} from 'react';
+import {
+  Portal,
+  Overlay,
+  Content,
+  Close,
+  Root
+} from '@radix-ui/react-dialog';
 import {IoCloseCircle} from "react-icons/io5";
 
-export const DialogContent = (props: { children: ReactNode }) => {
+export interface StandardDialogProps {
+  open: boolean;
+  onOpenChange: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+}
+
+export const StandardDialog = (props: StandardDialogProps) => {
   return (
-    <DialogPrimitive.Portal>
-      <DialogPrimitive.Overlay className="bg-gray-900 bg-opacity-60 inset-0 fixed z-40"/>
-      <DialogPrimitive.Content onOpenAutoFocus={(event) => {event.preventDefault()}}
-        className="z-40 overflow-scroll animate-scaleIn origin-[0%_0%] fixed top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 max-h-full rounded-md bg-brown-texture w-v90 md:w-v50">
-        {props.children}
-        <DialogPrimitive.Close className="absolute top-0 right-0 mt-1.5 mr-1.5" aria-label="Close"><IoCloseCircle
-          className="fill-green-500 hover:fill-green-600 focus:fill-green-600" size="1.5em"/></DialogPrimitive.Close>
-      </DialogPrimitive.Content>
-    </DialogPrimitive.Portal>
+    <Root open={props.open} onOpenChange={props.onOpenChange}>
+      <Portal>
+        <Overlay className="bg-gray-900 bg-opacity-60 inset-0 fixed z-40"/>
+        <Content onOpenAutoFocus={(event) => {event.preventDefault()}}
+                 className="z-40 overflow-scroll animate-scaleIn origin-[0%_0%] fixed top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 max-h-full rounded-md bg-brown-texture w-v90 md:w-v50">
+          {props.children}
+          <Close className="absolute top-0 right-0 mt-1.5 mr-1.5" aria-label="Close"><IoCloseCircle
+            className="fill-green-500 hover:fill-green-600 focus:fill-green-600" size="1.5em"/></Close>
+        </Content>
+      </Portal>
+    </Root>
   );
 }
-export const Dialog = DialogPrimitive.Root;
-export const DialogTrigger = DialogPrimitive.Trigger;
